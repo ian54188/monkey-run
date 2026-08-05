ff9e362575731a05b94065bea0070052bb8729d2
ian54188 <ian54188@outlook.com>
2026-08-05 14:02:14 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,228 @@
+let V = 0
+let mySprite = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`级别1`)
+mySprite.setPosition(10, 0)
+controller.moveSprite(mySprite)
+forever(function () {
+    if (controller.right.isPressed()) {
+        animation.runImageAnimation(
+        mySprite,
+        [img`
+            . . . . . . . f f f f f . . . . 
+            . . . . . . f e e e e e f . . . 
+            . . . . . f e e e d d d d f . . 
+            . . . . f f e e d f d d f d c . 
+            . . . f d d e e d f d d f d c . 
+            . . . c d b e e d d d d e e d c 
+            f f . c d b e e d d c d d d d c 
+            f e f . c f e e d d d c c c c c 
+            f e f . . f f e e d d d d d f . 
+            f e f . f e e e e f f f f f . . 
+            f e f f e e e e e e e f . . . . 
+            . f f e e e e f e f f e f . . . 
+            . . f e e e e f e f f e f . . . 
+            . . . f e f f b d f b d f . . . 
+            . . . f d b b d d c d d f . . . 
+            . . . f f f f f f f f f . . . . 
+            `,img`
+            . . . . . . . f f f f f . . . . 
+            . . . . . . f e e e e e f . . . 
+            . . . . . f e e e d d d d f . . 
+            . . . . . f e e d f d d f d c . 
+            . . . . f f e e d f d d f d c . 
+            . . . f d d e e d d d d e e d c 
+            . . . c d b e e d d c d d d d c 
+            f f . c d b e e e d d c c c c c 
+            f e f . c f f e e e d d d d f . 
+            f e f . f e e e e f f f f f f . 
+            f e f f e e e e e e e f f f f . 
+            . f f e e e e f e f d d f d d f 
+            . . f e e e e f e f b d f b d f 
+            . . f e f f f f f f f f f f f f 
+            . . f d d c f . . . . . . . . . 
+            . . f f f f . . . . . . . . . . 
+            `,img`
+            . . . . . . . f f f f f . . . . 
+            . . . . . . f e e e e e f . . . 
+            . . . . f f e e e d d d d f . . 
+            . . . f d d e e d d d d d d c . 
+            . . . c d b e e d f d d f d c . 
+            f f . c d b e e d f d d f d d c 
+            f e f . c f e e d d d d e e d c 
+            f e f . . f e e e d c d d d d c 
+            f e f . . f f e e e d c c c f . 
+            f e f . f e e e e f f f f f . . 
+            . f f f e e e e e e e f . . . . 
+            . . f e e e e f e e f e f f . . 
+            . . f e e e f f f e e f f e f . 
+            . f b f f f f f f c d d b d d f 
+            . f d d c f . . f d d d c d d f 
+            . . f f f . . . f f f f f f f . 
+            `,img`
+            . . . . . . . f f f f f . . . . 
+            . . . . f f f e e e e e f . . . 
+            . . . f d d e e e e d d d f . . 
+            . . . c d b e e e d d d d d c . 
+            . . . c d b e e d d d d d d c . 
+            . f f . c f e e d f d d f d d c 
+            f e f . . f e e d f d d f d d c 
+            f e f . . f e e d d d d e e d c 
+            f e f . . f f e e d c d d d f . 
+            f e f . f e e e e e d f f f . . 
+            . f f f e e e e e e e f . . . . 
+            . . f f b e e e e e f f . . . . 
+            . . f f d d c e e f f e f . . . 
+            . . . . f f f c d d b d d f . . 
+            . . . . . f f d d d c d d f . . 
+            . . . . . . f f f f f f f . . . 
+            `,img`
+            . . . . . . . f f f f f . . . . 
+            . . . . . . f e e e e e f . . . 
+            . . . . . f e e e d d d d f . . 
+            . . . . f f e e d f d d f d c . 
+            . . . f d d e e d f d d f d c . 
+            . . . c d b e e d d d d e e d c 
+            . . . c d b e e d d c d d d d c 
+            . . . . c f e e e d d c c c c c 
+            . . . . . f f e e e d d d d f . 
+            . . . . f e e e e f f f f f . . 
+            f f . f e e e e e e f f . . . . 
+            f e . f e e f e e f e e f . . . 
+            f e . f e e e f e e f e e f . . 
+            f e f f e f b b f b d f d b f . 
+            f f f f e b d d f d d f d d f . 
+            . f f f f f f f f f f f f f . . 
+            `],
+        100,
+        false
+        )
+        pause(400)
+    }
+})
+forever(function () {
+    if (controller.left.isPressed()) {
+        animation.runImageAnimation(
+        mySprite,
+        [img`
+            . . . . f f f f f . . . . . . . 
+            . . . f e e e e e f . . . . . . 
+            . . f d d d d e e e f . . . . . 
+            . c d f d d f d e e f f . . . . 
+            . c d f d d f d e e d d f . . . 
+            c d e e d d d d e e b d c . . . 
+            c d d d d c d d e e b d c . f f 
+            c c c c c d d d e e f c . f e f 
+            . f d d d d d e e f f . . f e f 
+            . . f f f f f e e e e f . f e f 
+            . . . . f e e e e e e e f f e f 
+            . . . f e f f e f e e e e f f . 
+            . . . f e f f e f e e e e f . . 
+            . . . f d b f d b f f e f . . . 
+            . . . f d d c d d b b d f . . . 
+            . . . . f f f f f f f f f . . . 
+            `,img`
+            . . . . f f f f f . . . . . . . 
+            . . . f e e e e e f . . . . . . 
+            . . f d d d d e e e f . . . . . 
+            . c d f d d f d e e f . . . . . 
+            . c d f d d f d e e f f . . . . 
+            c d e e d d d d e e d d f . . . 
+            c d d d d c d d e e b d c . . . 
+            c c c c c d d e e e b d c . f f 
+            . f d d d d e e e f f c . f e f 
+            . f f f f f f e e e e f . f e f 
+            . f f f f e e e e e e e f f e f 
+            f d d f d d f e f e e e e f f . 
+            f d b f d b f e f e e e e f . . 
+            f f f f f f f f f f f f e f . . 
+            . . . . . . . . . f c d d f . . 
+            . . . . . . . . . . f f f f . . 
+            `,img`
+            . . . . f f f f f . . . . . . . 
+            . . . f e e e e e f . . . . . . 
+            . . f d d d d e e e f f . . . . 
+            . c d d d d d d e e d d f . . . 
+            . c d f d d f d e e b d c . . . 
+            c d d f d d f d e e b d c . f f 
+            c d e e d d d d e e f c . f e f 
+            c d d d d c d e e e f . . f e f 
+            . f c c c d e e e f f . . f e f 
+            . . f f f f f e e e e f . f e f 
+            . . . . f e e e e e e e f f f . 
+            . . f f e f e e f e e e e f . . 
+            . f e f f e e f f f e e e f . . 
+            f d d b d d c f f f f f f b f . 
+            f d d c d d d f . . f c d d f . 
+            . f f f f f f f . . . f f f . . 
+            `,img`
+            . . . . f f f f f . . . . . . . 
+            . . . f e e e e e f f f . . . . 
+            . . f d d d e e e e d d f . . . 
+            . c d d d d d e e e b d c . . . 
+            . c d d d d d d e e b d c . . . 
+            c d d f d d f d e e f c . f f . 
+            c d d f d d f d e e f . . f e f 
+            c d e e d d d d e e f . . f e f 
+            . f d d d c d e e f f . . f e f 
+            . . f f f d e e e e e f . f e f 
+            . . . . f e e e e e e e f f f . 
+            . . . . f f e e e e e b f f . . 
+            . . . f e f f e e c d d f f . . 
+            . . f d d b d d c f f f . . . . 
+            . . f d d c d d d f f . . . . . 
+            . . . f f f f f f f . . . . . . 
+            `,img`
+            . . . . f f f f f . . . . . . . 
+            . . . f e e e e e f . . . . . . 
+            . . f d d d d e e e f . . . . . 
+            . c d f d d f d e e f f . . . . 
+            . c d f d d f d e e d d f . . . 
+            c d e e d d d d e e b d c . . . 
+            c d d d d c d d e e b d c . . . 
+            c c c c c d d e e e f c . . . . 
+            . f d d d d e e e f f . . . . . 
+            . . f f f f f e e e e f . . . . 
+            . . . . f f e e e e e e f . f f 
+            . . . f e e f e e f e e f . e f 
+            . . f e e f e e f e e e f . e f 
+            . f b d f d b f b b f e f f e f 
+            . f d d f d d f d d b e f f f f 
+            . . f f f f f f f f f f f f f . 
+            `],
+        100,
+        false
+        )
+        pause(400)
+    }
+})
+forever(function () {
+    scene.cameraFollowSprite(mySprite)
+})
+forever(function () {
+    if (controller.A.isPressed()) {
+        V += -0.1
+        mySprite.y += V
+    } else if (!(mySprite.tileKindAt(TileDirection.Bottom, sprites.builtin.oceanSand5) || (mySprite.tileKindAt(TileDirection.Bottom, sprites.builtin.oceanSand2) || (mySprite.tileKindAt(TileDirection.Bottom, sprites.builtin.oceanSand6) || (mySprite.tileKindAt(TileDirection.Bottom, sprites.builtin.oceanSand7) || (mySprite.tileKindAt(TileDirection.Bottom, sprites.builtin.oceanSand7) || (mySprite.tileKindAt(TileDirection.Bottom, sprites.builtin.oceanSand3) || mySprite.tileKindAt(TileDirection.Bottom, sprites.builtin.oceanSand1)))))))) {
+        V += 0.1
+        mySprite.y += V
+    } else {
+        V = 0
+    }
+})
